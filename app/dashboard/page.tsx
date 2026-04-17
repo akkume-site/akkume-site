cb2e73220c6a460e1062cf09fb80230e5d96c67a
akkume-site <gizmosgal@gmail.com>
2026-04-17 13:46:32 -0400

    Create page.tsx

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import AkkumeDashboard from '@/components/AkkumeDashboard';
+
+export default function DashboardPage() {
+  return (
+    <main className="mx-auto max-w-6xl px-4 py-8 sm:px-6">
+      <AkkumeDashboard />
+    </main>
+  );
+}
